6b1c528633f7c406765fcf7b5d8d2fd08b96c1ce
Siddharth gautam <siddharthgauts@gmail.com>
2023-02-14 19:38:15 +0530

    Updating the server URL

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000 ', {
+    const response = await fetch('https://chatgpt-2-0-ntn4.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
