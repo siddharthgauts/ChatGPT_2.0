81b7f6ca9328d8bfe5775e9262d6563828257db9
Siddharth gautam <siddharthgauts@gmail.com>
2023-02-14 19:14:51 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import  express  from "express"
+import * as  dotenv from 'dotenv'
+import cors from 'cors'
+import  { Configuration, OpenAIApi} from 'openai'
+
+dotenv.config()
+console.log(process.env.OPENAI_API_KEY)
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    organization: "org-bWzFnCQPA7Zj9PCfGUHyFrYT"
+
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+res.status(200).send({
+    message: 'Hello from GPT 2.O'
+})
+})
+
+app.post('/', async (req, res) => {
+    try{
+        const prompt =  req.body.prompt;
+        console.log(prompt);
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+           // stop: ["\"\"\""],
+        });
+        console.log(response);
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+
+    } catch(error){
+       console.error(error);
+       res.status(500).send(error || 'Something went wrong')
+    }  
+})
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))
